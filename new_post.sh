dc1d5840361b3f1478dbf09e2d1b7a55bfbb232f
Your Name <email@you.com>
2018-03-24 15:15:37 +0000

    sep dirs

---
+++ new_post.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: \n newpost.sh name-of-new-post'
+    exit 0
+fi
+
+
+filename=_posts/`date +%Y-%m-%d`-$1.md
+touch $filename
+
+cat >> $filename << EOF
+---
+layout: post
+title: "$1"
+author: "Alex Morley"
+date: `date +%Y-%m-%d`
+---
+EOF
+
+echo "Created post at $filename"
